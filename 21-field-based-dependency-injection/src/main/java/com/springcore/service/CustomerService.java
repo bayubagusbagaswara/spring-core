8e53729308bca2574abdb119720a95d4861f22cc
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-08 16:37:15 +0700

    Field Based Dependency Injection

---
+++ 21-field-based-dependency-injection/src/main/java/com/springcore/service/CustomerService.java
@@ -0,0 +1,14 @@
+package com.springcore.service;
+
+import com.springcore.repository.CustomerRepository;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerService {
+
+    @Getter
+    @Autowired
+    private CustomerRepository customerRepository;
+}
